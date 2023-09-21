72d4665011b6712299e841e07fb08e96754347e6
debiabril <debora.badosa@gmail.com>
2023-09-21 17:34:37 -0300

    responsive

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -13,7 +13,7 @@ export const Skills = () => {
             {
                 skillsJson?.map((skill,index) =>{
                     return(
-                    <div className='w-20 flex flex-col items-center justify-center mx-2 p-4' key={index}>
+                    <div className='w-20 flex flex-col items-center justify-end mr-3 sm:justify-center mx-2 p-4' key={index}>
                         <div className="radial-progress mb-1 bg-purple-800 text-primary-content border-4 border-purple-800"  style={{"--value":skill.porcentaje, "--thickness": "3px" }}>{skill.porcentaje}%</div>
                         <div className=' p-2 text-white rounded-lg flex  bg-black text-bold text-center font-bold '>{skill.nombre}</div>
                     </div>)
