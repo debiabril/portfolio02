18fbb2247d39c86ee893b5f5738cf5e5bbe1f9b6
debiabril <debora.badosa@gmail.com>
2023-09-21 17:47:54 -0300

    responsive

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 export const AboutMe = () => {
     return (
         <div className='bg-aboutme w-full min-h-screen max-h-[130vh] bg-cover bg-center flex justify-end sm:justify-center items-center'>
-            <div className='flex flex-col mr-5 justify-center items-center w-[70vw] sm:w-[50vw] min-h-screen mb-20 bg-white-100 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-md  bg-opacity-60 h-[100%] text-white'>
-                <p className='text-center md:font-bold text-purple-400 '>Antes que nada prefiero que me digan Debi💖! </p>
-                <p className='text-center md:font-bold text-blue-400'>Amo a mi gato Leo 😻, los colores 🌈, la música 🎵🎶 y bailar 💃.</p>
-                <p className='text-center md:font-bold text-lime-400'>Estando a unas materias de recibirme de licenciada en Fonoaudiología 🎓 vino la pandemia 😷 y con ella el comienzo del camino de conocerme ✨.</p>
-                <p className='text-center md:font-bold text-yellow-400'>Tengo la suerte de tener amigos que me conocían más que yo 💕 y fueron varios los que me dijeron que tenía que arrancar programación👩‍💻</p>
-                <p className='text-center md:font-bold text-orange-400'>Acá siento que puedo combinar muchas cosas que me encantan, entre ellas el trabajo en equipo y compañerismo🤝, el amor por los colores🌈 y la teconología💻🖥️🔌</p>
+            <div className='flex flex-col mr-5 justify-center items-center w-[70vw] sm:w-[50vw] min-h-screen mb-20 bg-white-100 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-lg  bg-opacity-70 h-[100%] text-white'>
+                <p className='text-center font-bold text-purple-400 '>Antes que nada prefiero que me digan Debi💖! </p>
+                <p className='text-center font-bold text-blue-400'>Amo a mi gato Leo 😻, los colores 🌈, la música 🎵🎶 y bailar 💃.</p>
+                <p className='text-center font-bold text-lime-400'>Estando a unas materias de recibirme de licenciada en Fonoaudiología 🎓 vino la pandemia 😷 y con ella el comienzo del camino de conocerme ✨.</p>
+                <p className='text-center font-bold text-yellow-400'>Tengo la suerte de tener amigos que me conocían más que yo 💕 y fueron varios los que me dijeron que tenía que arrancar programación👩‍💻</p>
+                <p className='text-center font-bold text-orange-400'>Acá siento que puedo combinar muchas cosas que me encantan, entre ellas el trabajo en equipo y compañerismo🤝, el amor por los colores🌈 y la teconología💻🖥️🔌</p>
             </div>
         </div>
     )
